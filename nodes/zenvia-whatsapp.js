e014c74e921441fb0b708813e17cb25f3074a4e7
Diego Lopes <diego-chez@hotmail.com>
2021-05-11 16:56:36 -0300

    Send whatsapp template message on input

--- nodes/zenvia-whatsapp.js
+++ nodes/zenvia-whatsapp.js
@@ -1,37 +1,53 @@
-const ZenviaError = require("./errors/zenvia-error")
-const zenviaSDK = require("@zenvia/sdk")
-const { zenvia } = require("../lib/constants")
+const ZenviaError = require("./errors/zenvia-error");
+const zenviaSDK = require("@zenvia/sdk");
+const { zenvia } = require("../lib/constants");
 
-module.exports = function(RED) {
-    "use strict";
+module.exports = function (RED) {
+  "use strict";
 
-    function ZenviaWhatsApp(n) {
-        RED.nodes.createNode(this, n)
-        const authentication = RED.nodes.getNode(n.authentication)
-        const { apiToken } = authentication.credentials 
-        RED.httpNode.get(`/credentials/zenvia-credentials/${n.authentication}/values`, (_, res) => {
-            res.send({ credentials: { apiToken } })
-        })
+  function ZenviaWhatsApp(n) {
+    RED.nodes.createNode(this, n);
+    const authentication = RED.nodes.getNode(n.authentication);
+    const { apiToken, senderId } = authentication.credentials;
 
-        this.on("input", (msg, send, done) => {
+    /**
+     * Provide access to credentials on interface
+     */
+    RED.httpNode.get(
+      `/credentials/zenvia-credentials/${n.authentication}/values`,
+      (_, res) => {
+        res.send({ credentials: { apiToken } });
+      }
+    );
 
-            try {
-                if(!n.template) throw new ZenviaError({ msg: "Template must be set" })
-                const fields = msg.payload
-                const templateId = n.template
-                const fieldsKeys = Object.keys(fields)
-                const [template] = n.templateList.filter(item => item.id === templateId)
-                const hasAllFields = template.fields.every(f => fieldsKeys.includes(f))
-                if(!hasAllFields) throw new ZenviaError({ msg: "Not all template fields are filled" })
-                msg.payload = { template: n.template, apiToken }
-                msg.payload = { template }
-                send(msg)
-                done()
-            } catch (error) {
-                done(error)
-            }
-        })
-    }
+    const client = new zenviaSDK.Client(apiToken);
+    const whatsapp = client.getChannel(zenvia.WHATSAPP_CHANNEL);
 
-    RED.nodes.registerType("zenvia whatsapp", ZenviaWhatsApp)
-}
+    this.on("input", async (msg, send, done) => {
+      try {
+        if (!n.template) throw new ZenviaError({ msg: "Template must be set" });
+        const recipient = msg.recipient;
+        const fields = msg.payload;
+        const templateId = n.template;
+        const fieldsKeys = Object.keys(fields);
+        const [template] = n.templateList.filter(
+          (item) => item.id === templateId
+        );
+        const hasAllFields = template.fields.every((f) =>
+          fieldsKeys.includes(f)
+        );
+        if (!hasAllFields)
+          throw new ZenviaError({ msg: "Not all template fields are filled" });
+        const content = new zenviaSDK.TemplateContent(templateId, fields);
+        const result = await whatsapp.sendMessage(senderId, recipient, content);
+        msg.payload = result;
+        send(msg);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  }
+
+  RED.nodes.registerType("zenvia whatsapp", ZenviaWhatsApp);
+};
